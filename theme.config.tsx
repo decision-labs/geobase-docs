f7dd0111a66dc754f878cabba9f15a5615faa386
Fernando Garcia <fergarram@gmail.com>
2024-08-07 12:40:19 -0600

    Site is ready to just add docs now.

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import GeobaseLogo from "./components/geobase-logo";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+	head: <link rel="icon" type="image/x-icon" href="https://geobase.app/favicon.ico" />,
+	primaryHue: {
+		dark: 152,
+		light: 152,
+	},
+	logo: (
+		<GeobaseLogo
+			style={{
+				width: "8rem",
+				height: "auto",
+			}}
+		/>
+	),
+	project: {
+		link: "https://github.com/decision-labs/geobase",
+	},
+	chat: {
+		link: "https://discord.com",
+	},
+	docsRepositoryBase: "https://github.com/decision-labs/geobase-docs",
+	footer: {
+		text: `Geobase.app © ${new Date().getFullYear()}`,
+	},
+};
 
-export default config
+export default config;
