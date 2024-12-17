09ed2677232ec168b3d59a0f04d0015e2caab193
Fernando Garcia <fergarram@gmail.com>
2024-12-17 13:43:06 -0600

    Added more info for tile server pages

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,20 +1,21 @@
 export default {
-	"index": "Quick Start",
+	index: "Quick Start",
 	"importing-data": "Importing Data",
-	"blueprints": "Blueprints",
-	"guides": "Guides",
-	"tileserver": "Tile Server",
-	"supabase": "Supabase Docs",
-	"signup": {
-		"title": "Sign Up ↗",
-		"type": "page",
-		"href": "https://geobase.app/",
-		"newWindow": true
+	blueprints: "Blueprints",
+	guides: "Guides",
+	tileserver: "Tile Server",
+	supabase: "Supabase Docs",
+	basics: "Basics",
+	signup: {
+		title: "Sign Up ↗",
+		type: "page",
+		href: "https://geobase.app/",
+		newWindow: true,
 	},
-	"blog": {
-		"title": "Blog ↗",
-		"type": "page",
-		"href": "https://geobase.app/blog",
-		"newWindow": true
-	}
-}
+	blog: {
+		title: "Blog ↗",
+		type: "page",
+		href: "https://geobase.app/blog",
+		newWindow: true,
+	},
+};
