cf1d700ee5fb5421ce6837428b0e5b5ab7678adc
Ryan Katayi <rkata73@gmail.com>
2024-12-24 12:00:32 +0200

    Add Svelte authentication metadata to software developer guide

--- pages/guides/software-developer/_meta.js
+++ pages/guides/software-developer/_meta.js
@@ -6,6 +6,7 @@ export default {
 	"geometry-types": "Geometry Types",
 	"auth-nextjs": "Authentication (Next.js)",
 	"auth-vue": "Authentication (Vue)",
+	"auth-svelte": "Authentication (Svelte)",
 	"realtime-nextjs": "Real-time (Next.js)",
 	"realtime-vue": "Real-time (Vue)",
 };
