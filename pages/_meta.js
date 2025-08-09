bcee3506ecdeda4e91137b398f79f4a23ab57116
Shoaib Burq <saburq@gmail.com>
2025-08-10 00:30:31 +0200

    add links to geoai

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,11 +1,30 @@
 export default {
 	index: "Quick Start",
+	geoai: {
+		title: "GeoAI ↗",
+		type: "page",
+		href: "https://docs.geobase.app/geoai",
+		newWindow: true,
+	},
+	"geoai-live": {
+		title: "GeoAI Live ↗",
+		type: "page",
+		href: "https://docs.geobase.app/geoai-live",
+		newWindow: true,
+	},
 	"importing-data": "Importing Data",
 	blueprints: "Blueprints",
 	guides: "Guides",
 	extensions: "Extensions",
 	tileserver: "Tile Server",
 	supabase: "Supabase Docs",
+	libraries: {
+		title: "Libraries",
+		type: "separator",
+	},
+	"geoai-js": {
+		title: "@geobase-js/geoai.js",
+	},
 	signup: {
 		title: "Sign Up ↗",
 		type: "page",
